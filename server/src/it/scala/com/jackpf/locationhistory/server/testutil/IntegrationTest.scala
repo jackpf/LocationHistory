2640cb80ed0ea1afbc460012f6e51e683e0634e5
Jack Farrelly <jack.farrelly@soundcloud.com>
2025-12-26 23:29:34 +0000

    Refactor

--- server/src/it/scala/com/jackpf/locationhistory/server/testutil/IntegrationTest.scala
+++ server/src/it/scala/com/jackpf/locationhistory/server/testutil/IntegrationTest.scala
@@ -12,30 +12,29 @@ import com.jackpf.locationhistory.server.testutil.{
   DefaultSpecification
 }
 import io.grpc.{ManagedChannel, ManagedChannelBuilder}
-import org.specs2.execute.{AsResult, Result}
-import org.specs2.specification.{AroundEach, BeforeAfter}
+import org.specs2.specification.After
 
 import java.util.concurrent.TimeUnit
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
-abstract class IntegrationTest extends DefaultSpecification with AroundEach {
+abstract class IntegrationTest extends DefaultSpecification {
   private val deviceRepo: DeviceRepo = new InMemoryDeviceRepo
   private val locationRepo: LocationRepo = new InMemoryLocationRepo
 
   TestServer.start(deviceRepo, locationRepo)
 
-  override def around[R: AsResult](r: => R): Result = {
+  private def resetState(): Unit = {
     Await.result(
       Future.sequence(Seq(deviceRepo.deleteAll(), locationRepo.deleteAll())),
       Duration.Inf
-    )
-
-    AsResult(r)
+    ): Unit
   }
 
-  trait IntegrationContext extends DefaultScope with BeforeAfter {
+  trait IntegrationContext extends DefaultScope with After {
+    resetState()
+
     val channel: ManagedChannel = ManagedChannelBuilder
       .forAddress("localhost", TestServer.TestPort)
       .usePlaintext()
@@ -44,15 +43,7 @@ abstract class IntegrationTest extends DefaultSpecification with AroundEach {
     val client: BeaconServiceGrpc.BeaconServiceBlockingStub =
       BeaconServiceGrpc.blockingStub(channel)
 
-    override def before: Any = {
-//      Await.result(
-//        Future.sequence(Seq(deviceRepo.deleteAll(), locationRepo.deleteAll())),
-//        Duration.Inf
-//      )
-    }
-
     override def after: Any = {
-      println("after")
       channel.shutdown().awaitTermination(10, TimeUnit.SECONDS)
     }
   }
