c6e098781d418cd10accba4b10a5b03341050c5e
Jack <jack.farrelly@soundcloud.com>
2025-12-27 19:11:59 +0000

    Docker server integration (#8)
    * Docker server integration
    
    * Buildx
    
    * Fix client internet permissions
    
    * Switch to okhttp
    
    * Publish on main

--- server/project/plugins.sbt
+++ server/project/plugins.sbt
@@ -1,2 +1,3 @@
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "2.2.0")
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.5.6")
 addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.14.5")
--- server/src/main/scala/com/jackpf/locationhistory/server/AppServer.scala
+++ server/src/main/scala/com/jackpf/locationhistory/server/AppServer.scala
@@ -3,20 +3,29 @@ package com.jackpf.locationhistory.server
 import com.jackpf.locationhistory.server.grpc.interceptors.LoggingInterceptor
 import com.jackpf.locationhistory.server.grpc.interceptors.LoggingInterceptor.LogLevel
 import com.jackpf.locationhistory.server.util.Logging
+import io.grpc.health.v1.HealthCheckResponse.ServingStatus
 import io.grpc.{Server, ServerBuilder, ServerServiceDefinition}
 
 import scala.jdk.CollectionConverters.*
+import io.grpc.protobuf.services.HealthStatusManager
 
 class AppServer(
     port: Int,
     services: ServerServiceDefinition*
 ) extends Logging {
+  private def healthManager: HealthStatusManager = {
+    val healthManager = new HealthStatusManager()
+    healthManager.setStatus("", ServingStatus.SERVING)
+    healthManager
+  }
+
   def start(): Server = {
     log.info(s"Listening on port ${port}")
 
     ServerBuilder
       .forPort(port)
       .intercept(new LoggingInterceptor(LogLevel.INFO))
+      .addService(healthManager.getHealthService)
       .addServices(services.asJava)
       .build()
       .start()
