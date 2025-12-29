03f3c097ec56ea2560da79775ddba29251f82e5a
Jack Farrelly <jack.farrelly@soundcloud.com>
2025-12-29 01:49:43 +0000

    Remove password

--- ui/src/grpc/admin-client.ts
+++ ui/src/grpc/admin-client.ts
@@ -2,11 +2,12 @@ import {createChannel, createClientFactory, Metadata} from 'nice-grpc-web'
 import {AdminServiceDefinition} from '../gen/admin-service'
 
 const proxyUrl = import.meta.env.VITE_PROXY_URL
+const adminPassword = import.meta.env.VITE_ADMIN_PASSWORD
 const channel = createChannel(proxyUrl)
 
 const clientFactory = createClientFactory()
     .use((call, options) => {
-        const authMetadata = Metadata({ 'authorization': 'Bearer admin' });
+        const authMetadata = Metadata({ 'authorization': 'Bearer ' + adminPassword });
 
         return call.next(call.request, {
             ...options,
