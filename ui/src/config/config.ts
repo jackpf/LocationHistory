da0c0d69774a6371aa7ccb83cbcaf0770979adda
Jack Farrelly <jack.farrelly@soundcloud.com>
2025-12-29 04:00:30 +0000

    Fixes

--- ui/src/config/config.ts
+++ ui/src/config/config.ts
@@ -1,6 +1,5 @@
 interface AppConfig {
     PROXY_URL: string;
-    ADMIN_PASSWORD?: string;
 }
 
 declare global {
@@ -10,4 +9,3 @@ declare global {
 }
 
 export const PROXY_URL = window.APP_CONFIG?.PROXY_URL || import.meta.env.VITE_PROXY_URL;
-export const ADMIN_PASSWORD = window.APP_CONFIG?.ADMIN_PASSWORD || import.meta.env.VITE_ADMIN_PASSWORD;
--- ui/src/grpc/admin-client.ts
+++ ui/src/grpc/admin-client.ts
@@ -1,15 +1,20 @@
 import {createChannel, createClientFactory, Metadata} from 'nice-grpc-web'
 import {AdminServiceDefinition} from '../gen/admin-service'
-import {ADMIN_PASSWORD, PROXY_URL} from "../config/config.ts";
+import {PROXY_URL} from "../config/config.ts";
 
 console.log("Connecting to proxy: ", PROXY_URL);
-console.log("Using password: ", !!ADMIN_PASSWORD);
+
+export const getAdminPassword = () => {
+    const adminPassword = localStorage.getItem('auth_token') || "";
+    console.log("Using password: ", !!adminPassword);
+    return adminPassword;
+};
 
 const channel = createChannel(PROXY_URL)
 
 const clientFactory = createClientFactory()
     .use((call, options) => {
-        const authMetadata = Metadata({ 'authorization': 'Bearer ' + ADMIN_PASSWORD });
+        const authMetadata = Metadata({ 'authorization': 'Bearer ' + getAdminPassword() });
 
         return call.next(call.request, {
             ...options,
