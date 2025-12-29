73606b6c0591759f934c3502d3cb4a267ea7ab70
Jack Farrelly <jack.farrelly@soundcloud.com>
2025-12-29 01:44:53 +0000

    Fix legacy location

--- client/app/src/main/java/com/jackpf/locationhistory/client/location/LocationProvider.java
+++ client/app/src/main/java/com/jackpf/locationhistory/client/location/LocationProvider.java
@@ -3,11 +3,13 @@ package com.jackpf.locationhistory.client.location;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
-import android.os.CancellationSignal;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+
+import androidx.annotation.NonNull;
 
 import com.jackpf.locationhistory.client.permissions.PermissionsManager;
 import com.jackpf.locationhistory.client.util.Logger;
@@ -18,13 +20,11 @@ import java.util.function.Consumer;
 
 public class LocationProvider {
     private static final int LOCATION_TIMEOUT_MS = 30000;
-    private final Context context;
     private final LocationManager locationManager;
     private final PermissionsManager permissionsManager;
     private final Logger log = new Logger(this);
 
     public LocationProvider(Context context, PermissionsManager permissionsManager) {
-        this.context = context;
         this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         this.permissionsManager = permissionsManager;
     }
@@ -49,31 +49,44 @@ public class LocationProvider {
         }
 
         final AtomicBoolean resultHandled = new AtomicBoolean(false);
-        CancellationSignal cancellationSignal = new CancellationSignal();
+
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                if (resultHandled.compareAndSet(false, true)) {
+                    log.d("Location data received from listener");
+                    consumer.accept(location);
+                    locationManager.removeUpdates(this);
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+            }
+        };
 
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
             if (resultHandled.compareAndSet(false, true)) {
-                log.w("Location request timed out, cancelling...");
-                cancellationSignal.cancel();
+                log.w("Location request timed out; using fallback location");
+                locationManager.removeUpdates(listener);
                 consumer.accept(fallbackLocation());
             }
         }, LOCATION_TIMEOUT_MS);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            locationManager.getCurrentLocation(
-                    LocationManager.GPS_PROVIDER,
-                    cancellationSignal,
-                    context.getMainExecutor(),
-                    location -> {
-                        if (resultHandled.compareAndSet(false, true)) {
-                            consumer.accept(location);
-                        }
-                    }
-            );
-        } else {
-            if (resultHandled.compareAndSet(false, true)) {
-                consumer.accept(fallbackLocation());
-            }
-        }
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                0,
+                0,
+                listener,
+                Looper.getMainLooper()
+        );
     }
 }
