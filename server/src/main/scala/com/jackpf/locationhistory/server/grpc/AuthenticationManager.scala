8ece422112b11d92a37c2e1d4b3810196579f501
Jack Farrelly <jack.farrelly@soundcloud.com>
2026-01-04 15:13:41 +0100

    Use salt

--- server/src/main/scala/com/jackpf/locationhistory/server/grpc/AuthenticationManager.scala
+++ server/src/main/scala/com/jackpf/locationhistory/server/grpc/AuthenticationManager.scala
@@ -4,15 +4,18 @@ import com.jackpf.locationhistory.server.grpc.AuthenticationManager.HashFunction
 
 import java.nio.charset.StandardCharsets
 import java.security.MessageDigest
+import java.util.UUID
 
 object AuthenticationManager {
   private val HashFunction: String = "SHA-256"
 }
 
 class AuthenticationManager(adminPassword: String) {
+  private val salt: String = UUID.randomUUID.toString
+
   private def hash(str: String): Array[Byte] = MessageDigest
     .getInstance(HashFunction)
-    .digest(str.getBytes(StandardCharsets.UTF_8))
+    .digest(salt.getBytes(StandardCharsets.UTF_8) ++ str.getBytes(StandardCharsets.UTF_8))
 
   private val adminPasswordHashed: Array[Byte] = hash(adminPassword)
 
