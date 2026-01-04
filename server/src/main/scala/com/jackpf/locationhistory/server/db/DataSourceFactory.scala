e4260f7f700dc128fb57750da4d4ec8dcecec8d4
Jack Farrelly <jack.farrelly@soundcloud.com>
2026-01-04 19:28:23 +0100

    Fix dataDir

--- server/src/main/scala/com/jackpf/locationhistory/server/db/DataSourceFactory.scala
+++ server/src/main/scala/com/jackpf/locationhistory/server/db/DataSourceFactory.scala
@@ -4,14 +4,14 @@ import com.jackpf.locationhistory.server.model.StorageType
 import org.sqlite.SQLiteDataSource
 import scalasql.core.DbClient
 
-class DataSourceFactory(dbDir: String, dbName: String) {
+class DataSourceFactory(dataDir: String, dbName: String) {
   def create(storageType: StorageType): Option[DbClient.DataSource] = storageType match {
     case StorageType.IN_MEMORY => None
     case StorageType.SQLITE    =>
       import scalasql.SqliteDialect.*
 
       val dataSource = new SQLiteDataSource()
-      dataSource.setUrl(s"jdbc:sqlite:/${dbDir}/${dbName}")
+      dataSource.setUrl(s"jdbc:sqlite:${dataDir}/${dbName}")
       Some(
         new DbClient.DataSource(
           dataSource,
