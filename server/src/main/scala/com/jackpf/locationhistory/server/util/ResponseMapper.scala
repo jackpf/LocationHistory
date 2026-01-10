4bf41d5947581d5a7fbf2ae13f096d15fa08afc9
Jack Farrelly <jack.farrelly@soundcloud.com>
2026-01-10 13:16:46 +0100

    With .toGrpcError

--- server/src/main/scala/com/jackpf/locationhistory/server/util/ResponseMapper.scala
+++ server/src/main/scala/com/jackpf/locationhistory/server/util/ResponseMapper.scala
@@ -2,12 +2,13 @@ package com.jackpf.locationhistory.server.util
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
+import com.jackpf.locationhistory.server.grpc.ErrorMapper.*
 
 object ResponseMapper {
   extension [T](response: Future[Try[T]]) {
     def toResponse[R](f: T => R)(using ec: ExecutionContext): Future[R] = response.flatMap {
       case Success(value)     => Future.successful(f(value))
-      case Failure(exception) => Future.failed(exception)
+      case Failure(exception) => Future.failed(exception.toGrpcError)
     }
   }
 }
