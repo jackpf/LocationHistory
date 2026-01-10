c1c0dce47800b8740416fe5503591d0e151070ce
Jack Farrelly <jack.farrelly@soundcloud.com>
2026-01-10 04:41:43 +0100

    Refactor

--- client/app/src/main/java/com/jackpf/locationhistory/client/ui/Toasts.java
+++ client/app/src/main/java/com/jackpf/locationhistory/client/ui/Toasts.java
@@ -6,6 +6,9 @@ import android.os.Looper;
 import android.widget.Toast;
 
 public class Toasts {
+    private Toasts() {
+    }
+
     public static void show(Context context, int resId, Object... formatArgs) {
         new Handler(Looper.getMainLooper()).post(() -> Toast.makeText(context, context.getString(resId, formatArgs), Toast.LENGTH_SHORT).show());
     }
