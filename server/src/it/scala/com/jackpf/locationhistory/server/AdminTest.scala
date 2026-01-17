e88fe512e210539d9b4c25904d2014f99fbed20f
Jack Farrelly <jack.farrelly@soundcloud.com>
2026-01-17 12:25:36 +0100

    Admin login ITs

--- server/src/it/scala/com/jackpf/locationhistory/server/AdminTest.scala
+++ server/src/it/scala/com/jackpf/locationhistory/server/AdminTest.scala
@@ -1,11 +1,30 @@
 package com.jackpf.locationhistory.server
 
-import com.jackpf.locationhistory.admin_service.ListDevicesRequest
-import com.jackpf.locationhistory.server.testutil.{GrpcMatchers, IntegrationTest}
+import com.jackpf.locationhistory.admin_service.{ListDevicesRequest, LoginRequest}
+import com.jackpf.locationhistory.server.testutil.{GrpcMatchers, IntegrationTest, TestServer}
 import io.grpc.Status.Code
 
 class AdminTest extends IntegrationTest with GrpcMatchers {
   "Admin" should {
+    "login endpoint" >> {
+      "login with correct password" >> in(new IntegrationContext {}) { context =>
+        val request = LoginRequest(password = TestServer.TestAdminPassword)
+
+        val response = context.unauthenticatedAdminClient.login(request)
+
+        response.token must not(beEmpty)
+      }
+
+      "fail login with incorrect password" >> in(new IntegrationContext {}) { context =>
+        val request = LoginRequest(password = "wrong-password")
+
+        context.unauthenticatedAdminClient.login(request) must throwAGrpcRuntimeException(
+          Code.UNAUTHENTICATED,
+          "Invalid password"
+        )
+      }
+    }
+
     "list devices endpoint" >> {
       "list devices" >> in(new IntegrationContext {}) { context =>
         val request = ListDevicesRequest()
