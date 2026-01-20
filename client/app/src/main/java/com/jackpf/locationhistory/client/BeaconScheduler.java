c53da67ad06b47e526d56483c35a86357d2b9426
Jack <jack.farrelly@soundcloud.com>
2026-01-20 18:37:30 +0100

    Use alarm manager for scheduling (#116)
    * Use alarm manager for scheduling
    
    * Boot receiver
    
    * Refactors

---
+++ client/app/src/main/java/com/jackpf/locationhistory/client/BeaconScheduler.java
@@ -0,0 +1,109 @@
+package com.jackpf.locationhistory.client;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.PowerManager;
+import android.os.SystemClock;
+
+import com.google.common.util.concurrent.AsyncCallable;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.jackpf.locationhistory.client.util.Logger;
+
+public class BeaconScheduler {
+    private final Logger log = new Logger(this);
+    private final AlarmManager alarmManager;
+    private final PowerManager.WakeLock wakeLock;
+    private final long wakelockTimeout;
+
+    private static final String WAKELOCK_TAG = "BeaconScheduler:wakelock";
+
+    public BeaconScheduler(AlarmManager alarmManager,
+                           PowerManager.WakeLock wakeLock,
+                           long wakelockTimeout) {
+        this.alarmManager = alarmManager;
+        this.wakeLock = wakeLock;
+        this.wakelockTimeout = wakelockTimeout;
+    }
+
+    public static BeaconScheduler create(Context context, long wakelockTimeout) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, WAKELOCK_TAG);
+        wakeLock.setReferenceCounted(false); // Ensure we don't double-acquire
+
+        return new BeaconScheduler(
+                alarmManager,
+                wakeLock,
+                wakelockTimeout
+        );
+    }
+
+    public <T> ListenableFuture<T> runWithWakeLock(AsyncCallable<T> taskFactory) {
+        acquireLock();
+
+        try {
+            ListenableFuture<T> taskFuture = taskFactory.call();
+            taskFuture.addListener(this::releaseLock, MoreExecutors.directExecutor());
+            return taskFuture;
+        } catch (Exception e) {
+            releaseLock();
+            return Futures.immediateFailedFuture(e);
+        }
+    }
+
+    private PendingIntent createPendingIntent(Context context, Class<?> cls, String action) {
+        Intent intent = new Intent(context, cls);
+        intent.setAction(action);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            return PendingIntent.getForegroundService(
+                    context,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+            );
+        } else {
+            return PendingIntent.getService(
+                    context,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT | (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? PendingIntent.FLAG_IMMUTABLE : 0)
+            );
+        }
+    }
+
+    private void schedulePendingIntent(PendingIntent pendingIntent, long delayMillis) {
+        long triggerAtMillis = SystemClock.elapsedRealtime() + delayMillis;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtMillis, pendingIntent);
+        } else {
+            alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtMillis, pendingIntent);
+        }
+    }
+
+    public void scheduleNext(Context context, Class<?> cls, String action, long delayMillis) {
+        log.d("Scheduling next task via alarm in %dms", delayMillis);
+
+        PendingIntent pendingIntent = createPendingIntent(context, cls, action);
+        schedulePendingIntent(pendingIntent, delayMillis);
+    }
+
+    private void acquireLock() {
+        if (!wakeLock.isHeld()) {
+            wakeLock.acquire(wakelockTimeout);
+        }
+    }
+
+    private void releaseLock() {
+        if (wakeLock.isHeld()) {
+            wakeLock.release();
+        }
+    }
+}
--- client/app/src/main/java/com/jackpf/locationhistory/client/BeaconService.java
+++ client/app/src/main/java/com/jackpf/locationhistory/client/BeaconService.java
@@ -1,12 +1,11 @@
 package com.jackpf.locationhistory.client;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ServiceInfo;
 import android.os.Build;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
 
 import androidx.annotation.NonNull;
@@ -18,6 +17,7 @@ import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.jackpf.locationhistory.client.config.ConfigRepository;
+import com.jackpf.locationhistory.client.permissions.AppRequirementsUtil;
 import com.jackpf.locationhistory.client.ui.Notifications;
 import com.jackpf.locationhistory.client.util.Logger;
 import com.jackpf.locationhistory.client.worker.BeaconResult;
@@ -29,47 +29,55 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class BeaconService extends Service {
-    private final Logger log = new Logger(this);
-    private HandlerThread handlerThread;
-    private Handler handler;
+    private static final Logger log = new Logger("BeaconService");
     private ExecutorService executorService;
     private ConfigRepository configRepository;
+    private BeaconScheduler beaconScheduler;
     private static final int PERSISTENT_NOTIFICATION_ID = 1;
+    private static final String ACTION_RUN_TASK = "com.jackpf.locationhistory.client.ACTION_BEACON_SERVICE";
+    private static final long WAKELOCK_TIMEOUT = TimeUnit.MINUTES.toMillis(10);
 
     private static final String START_MESSAGE = "Beacon task started";
     private static final String SUCCESS_MESSAGE = "Beacon task completed successfully";
     private static final String FAILED_MESSAGE = "Beacon task failed";
     private static final String RETRY_MESSAGE = "Beacon task failed with retry";
 
-    private final Runnable beaconTaskRunner = () -> {
-        log.i(START_MESSAGE);
-        log.appendEventToFile(BeaconService.this, START_MESSAGE);
+    private final Runnable beaconTask = () ->
+            beaconScheduler.runWithWakeLock(() -> {
+                log.i(START_MESSAGE);
+                log.appendEventToFile(BeaconService.this, START_MESSAGE);
 
-        ListenableFuture<BeaconResult> beaconResult = BeaconTask
-                .runSafe(BeaconService.this, executorService);
+                ListenableFuture<BeaconResult> beaconResult = BeaconTask
+                        .runSafe(BeaconService.this, executorService);
 
-        Futures.addCallback(beaconResult, new FutureCallback<BeaconResult>() {
-            @Override
-            public void onSuccess(BeaconResult result) {
-                log.i("%s: %s", SUCCESS_MESSAGE, result);
-                log.appendEventToFile(BeaconService.this, "%s: %s", SUCCESS_MESSAGE, result);
-                scheduleNext(regularDelayMillis());
-            }
+                Futures.addCallback(beaconResult, new FutureCallback<BeaconResult>() {
+                    @Override
+                    public void onSuccess(BeaconResult result) {
+                        log.i("%s: %s", SUCCESS_MESSAGE, result);
+                        log.appendEventToFile(BeaconService.this, "%s: %s", SUCCESS_MESSAGE, result);
+                        scheduleNext(regularDelayMillis());
+                    }
 
-            @Override
-            public void onFailure(@NonNull Throwable t) {
-                if (t instanceof RetryableException) {
-                    log.w(RETRY_MESSAGE, t);
-                    log.appendEventToFile(BeaconService.this, "%s: %s", RETRY_MESSAGE, t.getMessage());
-                    scheduleNext(retryDelayMillis());
-                } else {
-                    log.e(FAILED_MESSAGE, t);
-                    log.appendEventToFile(BeaconService.this, "%s: %s", FAILED_MESSAGE, t.getMessage());
-                    scheduleNext(regularDelayMillis());
-                }
-            }
-        }, ContextCompat.getMainExecutor(BeaconService.this));
-    };
+                    @Override
+                    public void onFailure(@NonNull Throwable t) {
+                        if (t instanceof RetryableException) {
+                            log.w(RETRY_MESSAGE, t);
+                            log.appendEventToFile(BeaconService.this, "%s: %s", RETRY_MESSAGE, t.getMessage());
+                            scheduleNext(retryDelayMillis());
+                        } else {
+                            log.e(FAILED_MESSAGE, t);
+                            log.appendEventToFile(BeaconService.this, "%s: %s", FAILED_MESSAGE, t.getMessage());
+                            scheduleNext(regularDelayMillis());
+                        }
+                    }
+                }, ContextCompat.getMainExecutor(BeaconService.this));
+
+                return beaconResult;
+            });
+
+    private void scheduleNext(long delayMillis) {
+        beaconScheduler.scheduleNext(this, BeaconService.class, ACTION_RUN_TASK, delayMillis);
+    }
 
     private final SharedPreferences.OnSharedPreferenceChangeListener configChangeListener = (prefs, key) -> {
         if (key != null &&
@@ -80,13 +88,6 @@ public class BeaconService extends Service {
             scheduleNext(500); // Debounce config changes with 500ms delay
         }
     };
-
-    private void scheduleNext(long delayMillis) {
-        log.d("Scheduling next beacon task in %dms", delayMillis);
-
-        handler.removeCallbacks(beaconTaskRunner);
-        handler.postDelayed(beaconTaskRunner, delayMillis);
-    }
 
     private long regularDelayMillis() {
         return TimeUnit.MINUTES.toMillis(configRepository.getUpdateIntervalMinutes());
@@ -106,13 +107,12 @@ public class BeaconService extends Service {
     public void onCreate() {
         super.onCreate();
 
-        handlerThread = new HandlerThread("ServiceTimer");
-        handlerThread.start();
-        handler = new Handler(handlerThread.getLooper());
         executorService = Executors.newSingleThreadExecutor();
 
         configRepository = new ConfigRepository(this);
         configRepository.registerOnSharedPreferenceChangeListener(configChangeListener);
+
+        beaconScheduler = BeaconScheduler.create(this, WAKELOCK_TIMEOUT);
     }
 
     @Override
@@ -120,14 +120,14 @@ public class BeaconService extends Service {
         super.onStartCommand(intent, flags, startId);
 
         Notifications notifications = new Notifications(this);
-
         ServiceCompat.startForeground(this,
                 PERSISTENT_NOTIFICATION_ID,
                 notifications.createPersistentNotification(getString(R.string.persistent_notification_title), getString(R.string.persistent_notification_message)),
                 Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q ? ServiceInfo.FOREGROUND_SERVICE_TYPE_LOCATION : 0);
 
-        handler.removeCallbacks(beaconTaskRunner);
-        handler.post(beaconTaskRunner);
+        if (intent == null || ACTION_RUN_TASK.equals(intent.getAction())) {
+            beaconTask.run();
+        }
 
         return START_STICKY;
     }
@@ -136,10 +136,24 @@ public class BeaconService extends Service {
     public void onDestroy() {
         super.onDestroy();
 
-        if (handler != null) handler.removeCallbacks(beaconTaskRunner);
-        if (handlerThread != null) handlerThread.quitSafely();
         if (executorService != null) executorService.shutdown();
         if (configRepository != null)
             configRepository.unregisterOnSharedPreferenceChangeListener(configChangeListener);
+    }
+
+    public static void startForeground(Context context) {
+        log.d("Starting service");
+        Intent intent = new Intent(context, BeaconService.class);
+        intent.setAction(ACTION_RUN_TASK);
+        ContextCompat.startForegroundService(context, intent);
+    }
+
+    public static void startForegroundIfPermissionsGranted(Context context) {
+        if (AppRequirementsUtil.allGranted(context, AppRequirements.getRequirements(context))) {
+            log.d("Permissions granted");
+            startForeground(context);
+        } else {
+            log.w("Permissions not yet granted");
+        }
     }
 }
